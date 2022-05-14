c304e2814af6f4576c10d91be1605c8115ff9497
ilija-jankovic <58715208+ilija-jankovic@users.noreply.github.com>
2022-05-14 23:57:19 +1200

    CFB encryption functional

--- part2.py
+++ part2.py
@@ -5,6 +5,8 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from matplotlib.pyplot import imread
 import binascii
+from PIL import Image
+from io import BytesIO
 
 image= imread('Image-Assignment2.bmp')
 
@@ -14,29 +16,27 @@ key = binascii.unhexlify('770A8A65DA156D24EE2A093277530142')
 #AES.MODE_CBC
 #AES.MODE_CFB
 
-cipher = AES.new(key, AES.MODE_EAX)
-bytes = image.tobytes()
-left_over = len(bytes)%16
-c_bytes = []
+cipher = AES.new(key, AES.MODE_CFB)
+p_bytes = image.tobytes()
+left_over = len(p_bytes)%16
+c_bytes = bytearray()
 
 def get_c_bytes(block):
-    c_block = cipher.encrypt(block)
-    c_bytes = []
-    for i in range(0, 16):
-        c_bytes.append(c_block[i:i+1])
-    return c_bytes
+    return bytearray(cipher.encrypt(block)) 
 
-def sub_bytes(bytes, len):
-    return bytes[0:len]
+def sub_bytes(p_bytes, len):
+    return p_bytes[0:len]
 
-for i in range(0, len(bytes), 16):
-    block = bytes[i*16:(i+1)*16]
+for i in range(0, len(p_bytes), 16):
+    block = p_bytes[i:i+16]
     c_bytes += get_c_bytes(block)
 
 if(left_over > 0):
-    block = pad(bytes[len(bytes)-left_over:len(bytes)], AES.block_size)
+    block = pad(p_bytes[len(p_bytes)-left_over:len(p_bytes)], AES.block_size)
     c_bytes += get_c_bytes(block)
 
-c_bytes = sub_bytes(c_bytes, len(bytes))
+c_bytes = sub_bytes(c_bytes, len(p_bytes))
 
-print(len(bytes), len(c_bytes))
+print(len(p_bytes), len(c_bytes))
+image = Image.frombytes('RGB', (image.shape[1], image.shape[0]), bytes(c_bytes))
+image.save('cfb.jpg')
