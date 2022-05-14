201462f1891fb8383368ceefa3bb9ab1f7f365af
ilija-jankovic <58715208+ilija-jankovic@users.noreply.github.com>
2022-05-15 01:33:26 +1200

    Commented part2.py

--- part2.py
+++ part2.py
@@ -1,5 +1,4 @@
-# MacOS: source venv/bin/activate
-
+# Checks imports incase venv was not activated.
 try:
     from Crypto.Cipher import AES
     from Crypto.Util.Padding import pad
@@ -15,12 +14,14 @@ def verbose_exit():
     print('Usage: python part2.py [input path] [output path]')
     exit(0)
 
+# Checks for correct number of non-empty arguments.
 if(len(sys.argv) < 3 or len(sys.argv[1]) == '' or len(sys.argv[2]) == ''):
     print('Please enter the path of the BMP file to encrypt and a directory to save the encrypted JPG outputs.')
     verbose_exit()
 
 bmp_path = sys.argv[1]
 
+# Ensures only a BMP file can be selected as the input.
 tokens = bmp_path.split('.')
 if(tokens[len(tokens)-1].lower() != 'bmp'):
     print('The input path must point to a BMP file.')
@@ -32,14 +33,17 @@ output_path = sys.argv[2]
 if output_path[len(output_path)-1] == '/':
     output_path = output_path[0:len(output_path-1)]
 
+# The image file attempts to be read as a numpy array.
 try:
     image = imread(bmp_path)
 except:
     print(f'Could not load BMP file {bmp_path}. Please try restarting the program with a valid path.')
     verbose_exit()
 
+# Key is constant as per the assignment specifications.
 key = binascii.unhexlify('770A8A65DA156D24EE2A093277530142')
 
+# A class to manage AES image encryption logic. Useful to be explictly defined for different AES modes.
 class AESImageEncryption:
     def __update_params(self, mode):
         self.__mode = mode;
@@ -53,15 +57,23 @@ class AESImageEncryption:
     def change_mode(self, mode):
         self.__update_params(mode)
 
+    # Creates and returns the encrypted the image based on the mode parameter.
     def encrypt(self):
+        # If the mode is not CFB, the data must be padded with the AES block size of 128 bits.
         if(self.__mode != AES.MODE_CFB):
             c_bytes = self.__cipher.encrypt(pad(self.__p_bytes, AES.block_size))
+
+            # The padded bytes are stripped away as these would interfere with image creation.
             left_over = len(self.__p_bytes) % 16
             c_bytes = c_bytes[0:len(c_bytes)-left_over]
         else:
             c_bytes = self.__cipher.encrypt(self.__p_bytes)
+        
+        # The image is created based on the rows and columns of the original image and the ciphertext bytes.
         return Image.frombytes('RGB', (image.shape[1], image.shape[0]), bytes(c_bytes))
 
+# Attempts to encrypt and output three JPG files, based on modes ECB, CBC, and CFB.
+# If an error occurs, the output path was most likely incorrect.
 try:
     image_enc = AESImageEncryption(key, image, AES.MODE_ECB)
     c_image = image_enc.encrypt()
