526adb13ca48ab37eb9326119f024e59fb99bb68
ilija-jankovic <58715208+ilija-jankovic@users.noreply.github.com>
2022-05-15 01:24:52 +1200

    Added error catching

--- part2.py
+++ part2.py
@@ -1,14 +1,44 @@
 # MacOS: source venv/bin/activate
-# https://nitratine.net/blog/post/python-encryption-and-decryption-with-pycryptodome/
 
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-from matplotlib.pyplot import imread
-import binascii
-from PIL import Image
+try:
+    from Crypto.Cipher import AES
+    from Crypto.Util.Padding import pad
+    from matplotlib.pyplot import imread
+    import binascii
+    from PIL import Image
+    import sys
+except:
+    print('Please go to the source directory and type\nsource venv/bin/activate\nto activate the virtual environment.')
+    exit()
+    
+def verbose_exit():
+    print('Usage: python part2.py [input path] [output path]')
+    exit(0)
+
+if(len(sys.argv) < 3 or len(sys.argv[1]) == '' or len(sys.argv[2]) == ''):
+    print('Please enter the path of the BMP file to encrypt and a directory to save the encrypted JPG outputs.')
+    verbose_exit()
+
+bmp_path = sys.argv[1]
+
+tokens = bmp_path.split('.')
+if(tokens[len(tokens)-1].lower() != 'bmp'):
+    print('The input path must point to a BMP file.')
+    verbose_exit()
+
+output_path = sys.argv[2]
+
+# Remove slash to format directory for output.
+if output_path[len(output_path)-1] == '/':
+    output_path = output_path[0:len(output_path-1)]
+
+try:
+    image = imread(bmp_path)
+except:
+    print(f'Could not load BMP file {bmp_path}. Please try restarting the program with a valid path.')
+    verbose_exit()
 
 key = binascii.unhexlify('770A8A65DA156D24EE2A093277530142')
-image = imread('Image-Assignment2.bmp')
 
 class AESImageEncryption:
     def __update_params(self, mode):
@@ -32,14 +62,18 @@ class AESImageEncryption:
             c_bytes = self.__cipher.encrypt(self.__p_bytes)
         return Image.frombytes('RGB', (image.shape[1], image.shape[0]), bytes(c_bytes))
 
-image_enc = AESImageEncryption(key, image, AES.MODE_ECB)
-c_image = image_enc.encrypt()
-c_image.save('ecb.jpg')
+try:
+    image_enc = AESImageEncryption(key, image, AES.MODE_ECB)
+    c_image = image_enc.encrypt()
+    c_image.save(f'{output_path}/ecb.jpg')
 
-image_enc.change_mode(AES.MODE_CBC)
-c_image = image_enc.encrypt()
-c_image.save('cbc.jpg')
+    image_enc.change_mode(AES.MODE_CBC)
+    c_image = image_enc.encrypt()
+    c_image.save(f'{output_path}/cbc.jpg')
 
-image_enc.change_mode(AES.MODE_CFB)
-c_image = image_enc.encrypt()
-c_image.save('cfb.jpg')
+    image_enc.change_mode(AES.MODE_CFB)
+    c_image = image_enc.encrypt()
+    c_image.save(f'{output_path}/cfb.jpg')
+except:
+    print(f'Could not save image files at {output_path}.')
+    verbose_exit()
